f51a0e0e4705086755eda157903df69e253abf1d
Souhardya Gayen <64001518+Souhardyagayen99@users.noreply.github.com>
2024-06-27 17:08:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,34 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-
-# Prediction function
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 7)
-    loaded_model = pickle.load(open("model1.pkl", "rb"))
-    result = loaded_model.predict(to_predict)
-    return result[0]
-
+# prediction function 
+def ValuePredictor(to_predict_list): 
+    to_predict = np.array(to_predict_list).reshape(1, 7) 
+    loaded_model = pickle.load(open("model1.pkl", "rb")) 
+    result = loaded_model.predict(to_predict) 
+    return result[0]     
+    
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
     if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
+        to_predict_list = request.form.to_dict() 
+        to_predict_list = list(to_predict_list.values()) 
         to_predict_list = list(map(float, to_predict_list))
         result = ValuePredictor(to_predict_list)
-        if int(result) == 1:
-            prediction = 'Given transaction is fraudulent'
-        else:
-            prediction = 'Given transaction is NOT fraudulent'
-        return render_template("result.html", prediction=prediction)
-
+    if int(result)== 1:
+        prediction ='Given transaction is fradulent'
+    else:
+        prediction ='Given transaction is NOT fradulent'            
+    return render_template("result.html", prediction = prediction) 
+    
+    
+    
+    
+    
+                  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
